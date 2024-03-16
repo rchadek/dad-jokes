a6d48759cabe61db9edbae3e29116b47da4f11a6
rchadek <ryan.chadek@gmail.com>
2024-03-16 12:06:54 -0500

    adding images and mobile styles

--- src/app/page.js
+++ src/app/page.js
@@ -14,8 +14,6 @@ export default function Home({ localData }) {
   const { data, error } = useDadJokes(); // Using the custom hook
 
   const fetchData = async () => {
-    // console.log(data);
-    // console.log(data.jokes[333]);
 
     const min = 0;
     const max = data.jokes.length;
@@ -30,19 +28,54 @@ export default function Home({ localData }) {
   return (
     <main className="flex gap-16 min-h-screen flex-col items-center justify-center p-24">
 
-      <div>
-        <h1 className="text-4xl font-bold">Welcome to the Dad Joke Generator</h1>
+      <div className="grid lg:max-w-8xl lg:w-full">
+        <div className="text-center">
+          <h1 className="text-4xl font-bold">Welcome to the Dad Joke Generator</h1>
+        </div>     
       </div>
 
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0">
-        <p className="text-xl">{dadJoke}</p>
+      <div className="mb-32 grid grid-cols-9 text-center lg:max-w-8xl lg:w-full lg:mb-0">
+        <div className="col-span-2 hidden xl:flex">
+          <Image
+            src="/dad-jokes-generator-dad-600x600-01.jpg"
+            width={600}
+            height={600}
+            alt=""
+          />
+        </div>
+        <div className="col-span-9 xl:col-span-5 px-0 xl:px-52 py-0 xl:py-24">
+          <p className="text-xl mb-6">{dadJoke}</p>
+          <button className="mt-6 p-5 bg-indigo-500 hover:bg-black text-white	rounded" onClick={fetchData}>Generate Dad Joke</button>
+        </div>
+        <div className="col-span-2 hidden xl:flex">
+          <Image
+              src="/dad-jokes-generator-teenager-600x600-01.jpg"
+              width={600}
+              height={600}
+              alt=""
+          />
+        </div>
+        <div className="grid grid-cols-8 justify-center col-span-9 mt-10 text-center">
+          <div className="col-span-1"></div>
+          <div className="col-span-3 flex xl:hidden">
+            <Image
+            src="/dad-jokes-generator-dad-600x600-01.jpg"
+            width={600}
+            height={600}
+            alt=""
+            />
+          </div>
+          <div className="col-span-3 flex xl:hidden">
+            <Image
+                src="/dad-jokes-generator-teenager-600x600-01.jpg"
+                width={600}
+                height={600}
+                alt=""
+            />
+          </div>
+          <div className="col-span-1"></div>
+        </div>
       </div>
-
-      <div className="relative flex place-items-center">
-        <button className="p-3 bg-indigo-500 text-white	rounded" onClick={fetchData}>Generate Dad Joke</button>
-      </div>
-
-      
 
     </main>
   );
